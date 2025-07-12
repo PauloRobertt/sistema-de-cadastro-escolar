cf12602876073018947ab2326c51393a2b0d1787
paulorobertt <paulorobert5776@gmail.com>
2025-07-12 02:47:03 -0300

    feat(back-end): implementa método index no HomeController com criação de aluno no banco

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,1 +1,19 @@
-class HomeController { }
+import Aluno from '../models/Aluno.js';
+
+class HomeController {
+  async index(req, res) {
+    console.log('Controller');
+    const novoAluno = await Aluno.create({
+      nome: 'TesteNome',
+      sobrenome: 'TesteSobrenome',
+      email: 'TesteEmail',
+      idade: 23,
+      foto: 'URLFoto',
+    });
+    console.log(novoAluno);
+
+    res.json(novoAluno);
+  }
+}
+
+export default new HomeController();
