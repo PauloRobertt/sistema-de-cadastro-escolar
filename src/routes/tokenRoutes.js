fda8839993e0c534b145b53279e5144a9ecc443b
paulorobertt <paulorobert5776@gmail.com>
2025-07-18 03:26:40 -0300

    feat(back-end): cria TokenController e rota /tokens para autenticação com JWT

---
+++ src/routes/tokenRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import TokenController from '../controllers/TokenController.js';
+
+const router = express.Router();
+
+router.post('/', TokenController.store);
+
+export default router;
