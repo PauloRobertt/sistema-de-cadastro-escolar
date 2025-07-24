9f8af5af5178cd42f88f88b9efe1dd5d7ddb5e33
paulorobertt <paulorobert5776@gmail.com>
2025-07-24 02:17:06 -0300

    feat(back-end): adiciona validações e mensagens de erro customizadas ao model Aluno

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -1,16 +1,69 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      foto: Sequelize.STRING,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nome não pode ficar vazio!',
+          },
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres!',
+          },
+        },
+      },
+
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nome não pode ficar vazio!',
+          },
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres!',
+          },
+        },
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail já em uso!',
+        },
+        validate: {
+          isEmail: {
+            msg: 'E-mail invalido!',
+          },
+        },
+      },
+
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro!',
+          },
+        },
+      },
+
+      foto: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+
     }, {
       sequelize,
     });
+
     return this;
   }
 }
