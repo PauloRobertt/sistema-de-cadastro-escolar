54e2e7036feb657bd213ac6c6be190cf66db5e87
paulorobertt <paulorobert5776@gmail.com>
2025-08-12 01:51:30 -0300

    chore(front-end): configuracoes de cores para aplicacao

---
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/config/colors.js
@@ -0,0 +1,6 @@
+export const primaryColor = '#50cb77';
+export const primaryDarkColor = '#175e35';
+export const backgroundColor = '#e0e0e0';
+
+export const inputColor = '#f4f4fc';
+export const inputTextColor = '#88888f';
