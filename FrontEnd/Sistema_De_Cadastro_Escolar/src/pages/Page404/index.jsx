3c3f03189966ca5c7b22ffcef5d572bff06815cf
paulorobertt <paulorobert5776@gmail.com>
2025-08-14 00:31:54 -0300

    chore(front-end): ajuste no design da página erro 404

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Page404/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Page404/index.jsx
@@ -1,14 +1,29 @@
-import { ContainerPage404, Background } from './styled';
+import SubmitButton from '../../components/SubmitButton';
+import {
+  ContainerPage404,
+  Background,
+  ContentPage404,
+  Image404,
+} from './styled';
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 
 export default function Page404() {
   return (
     <Background>
       <ContainerPage404>
-        <h1>
-          <span>4</span>0<span>4</span>
-        </h1>
-        <h2>Página não encontrada!</h2>
-        <h3>Parece que a página que você está procurando não existe.</h3>
+        <Image404 />
+        <ContentPage404>
+          <h1>404</h1>
+          <h2>Ops! Parece que você se perdeu…</h2>
+          <h3>Não encontramos o que você procurava.</h3>
+          <SubmitButton
+            text={
+              <>
+                <FaLongArrowAltLeft size={24} /> Voltar para página inicial
+              </>
+            }
+          />
+        </ContentPage404>
       </ContainerPage404>
     </Background>
   );
