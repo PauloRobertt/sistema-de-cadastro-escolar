7fd819e127a17360a65e1e373de314f6c038a606
paulorobertt <paulorobert5776@gmail.com>
2025-08-14 00:37:55 -0300

    chore(front-end): implementacao de duas novas cores principais

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/config/colors.js
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/config/colors.js
@@ -1,5 +1,7 @@
 export const primaryColor = '#50cb77';
 export const primaryDarkColor = '#175e35';
+export const secundaryColor = '#ffb703';
+export const secundaryDarkColor = '#a87a04ff';
 export const backgroundColor = '#e0e0e0';
 
 export const inputColor = '#f4f4fc';
