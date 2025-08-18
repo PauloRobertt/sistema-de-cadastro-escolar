2a41bbfb4b209fabffd21769781cf5941bf94f4c
paulorobertt <paulorobert5776@gmail.com>
2025-08-18 11:50:01 -0300

    chore(front-end): adicao de element RegisterPage

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/Routes/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/Routes/index.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LoginPage from '../pages/Login';
+import RegisterPage from '../pages/Register';
 import Page404 from '../pages/Page404';
+import PrivateRoute from './PrivateRoute';
 
 export default function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" />
+        <Route path="/" />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
