c7e87b7b22f38dc81650780fff7fdf750bcc874f
paulorobertt <paulorobert5776@gmail.com>
2025-08-25 18:04:10 -0300

    feat(frontend): adicionar imagem de loading e gerenciar assets

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/assets/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/assets/index.jsx
@@ -1,9 +1,11 @@
 import ImagemLogin from './imgs/TelaLogin.png';
 import Image404 from './imgs/Image404.png';
 import ImageRegister from './imgs/TelaRegister.png';
+import ImageLoading from './imgs/loading.svg';
 
 export const OrganizarImagens = {
   ImagemLogin: ImagemLogin,
   Image404: Image404,
   ImageRegister: ImageRegister,
+  ImageLoading: ImageLoading,
 };
