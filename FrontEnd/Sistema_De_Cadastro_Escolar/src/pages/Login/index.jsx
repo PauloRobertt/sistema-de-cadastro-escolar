5830defc2135cdc88e9908aabaa841bfe005c607
paulorobertt <paulorobert5776@gmail.com>
2025-08-26 13:17:44 -0300

    feat(front-end): implementado sistema de login

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Login/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Login/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../service/axios';
 import {
   LoginWrapper,
   LoginFormWrapper,
@@ -10,6 +13,28 @@ import LinkButton from '../../components/LinkButton';
 import { Container } from '../../styles/GlobalStyles';
 
 export default function index() {
+  const [user, userState] = useState({});
+  const navigate = useNavigate();
+
+  const submit = (e) => {
+    e.preventDefault();
+    login(user);
+  };
+
+  async function login(user) {
+    try {
+      const response = await axios.post('/tokens/', user);
+      console.log('Logado');
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleOnChange(e) {
+    userState({ ...user, [e.target.id]: e.target.value });
+  }
+
   return (
     <Container>
       <LoginWrapper>
@@ -17,29 +42,27 @@ export default function index() {
           <LoginFormWrapper>
             <h1>Bem-Vindo</h1>
             <h3>Digite seu e-mail e senha para acessar sua conta.</h3>
-            <form action="" method="post">
+            <form onSubmit={submit} method="post">
               <Input
                 id="email"
                 label="E-mail"
                 type="email"
                 placeholder="Digite seu e-mail"
-                minLength={3}
-                maxLength={100}
+                onChange={handleOnChange}
               />
               <Input
                 id="password"
                 label="Senha"
                 type="password"
                 placeholder="Digite sua senha"
-                minLength={6}
-                maxLength={50}
+                onChange={handleOnChange}
               />
-              <p>Esqueceu sua senha?</p>
-              <SubmitButton text="Login" />
+              <LinkButton path="" text={'Esqueceu sua senha?'} />
+              <SubmitButton type="submit" text="Login" />
             </form>
             <p>
-              Não tem uma conta?{' '}
-              <LinkButton path="/register" text="Cadastre-se" />
+              Não tem uma conta?
+              <LinkButton path="/register" text=" Cadastre-se" />
             </p>
           </LoginFormWrapper>
         </LoginContent>
