d0e72fc06904d8e03699c215d9485f413fd998ca
paulorobertt <paulorobert5776@gmail.com>
2025-08-28 21:13:33 -0300

    feat(front-end): implementaca de icon

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/assets/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/assets/index.jsx
@@ -2,8 +2,10 @@ import ImagemLogin from './imgs/TelaLogin.png';
 import Image404 from './imgs/Image404.png';
 import ImageRegister from './imgs/TelaRegister.png';
 import ImageLoading from './imgs/loading.svg';
+import IconStudent from './imgs/academico.png';
 
 export const OrganizarImagens = {
+  IconStudent: IconStudent,
   ImagemLogin: ImagemLogin,
   Image404: Image404,
   ImageRegister: ImageRegister,
