5e0881afe77be2d5a69b1c1385467b02ce0f3785
paulorobertt <paulorobert5776@gmail.com>
2025-09-01 17:42:26 -0300

    feat(front-end): remocao do label no componente Input

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/components/Input/input.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/components/Input/input.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react';
-import { ContainerInput, DivInput, InputButton } from './styled';
+
+//Componentes
+import { ContainerInput, InputButton } from './styled';
+
+//Icons
 import { FaLock } from 'react-icons/fa';
 import { FaLockOpen } from 'react-icons/fa';
 
-export default function Input({
-  id,
-  label,
-  type,
-  placeholder,
-  onChange,
-  minLength,
-  maxLength,
-}) {
+export default function Input({ id, type, placeholder, value, onChange }) {
   const [showPassword, setShowPassword] = useState(false);
   const [input, setInput] = useState(type);
 
@@ -29,20 +25,16 @@ export default function Input({
 
   return (
     <ContainerInput>
-      <label>{label}</label>
-      <DivInput>
-        <input
-          id={id}
-          type={input}
-          placeholder={placeholder}
-          onChange={onChange}
-          minLength={minLength}
-          maxLength={maxLength}
-        />
-        <InputButton showbutton={id} onClick={handleShowPassword} type="button">
-          {showPassword ? <FaLockOpen /> : <FaLock />}
-        </InputButton>
-      </DivInput>
+      <input
+        id={id}
+        type={input}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      <InputButton showbutton={id} onClick={handleShowPassword} type="button">
+        {showPassword ? <FaLockOpen /> : <FaLock />}
+      </InputButton>
     </ContainerInput>
   );
 }
