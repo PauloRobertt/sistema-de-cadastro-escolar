7cc1d4f96369226f55062301321021c8dabe51a2
paulorobertt <paulorobert5776@gmail.com>
2025-09-10 01:10:47 -0300

    feat(front-end): criacao de componente AddAlunoForm para melhorar a organizacao do codigo

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Alunos/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Alunos/index.jsx
@@ -2,18 +2,13 @@ import { useEffect, useState } from 'react';
 import axios from '../../service/axios';
 
 //Estilos
-import {
-  TableAlunos,
-  HeaderContainer,
-  ContainerNewAluno,
-  ContainerInput,
-} from './styled';
+import { TableAlunos, HeaderContainer } from './styled';
 import { Container, ContainerConteudo } from '../../styles/GlobalStyles';
 
 //Componentes
 import Menu from '../../layouts/Menu/input';
 import SubmitButton from '../../components/SubmitButton';
-import Input from '../../components/Input/input';
+import AddAlunoForm from '../../components/AddAlunoForm';
 
 //Icons
 import { FaUserCircle } from 'react-icons/fa';
@@ -38,28 +33,9 @@ export default function AlunosPage() {
     getAlunos();
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!aluno.id) {
-      await addAluno(aluno);
-    }
-  };
-
-  const handleOnChange = (e) => {
-    setAluno({ ...aluno, [e.target.id]: e.target.value });
-  };
-
   const handleDelete = async (id) => {
     try {
       await axios.delete(`/alunos/${id}`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const addAluno = async (aluno) => {
-    try {
-      await axios.post('/alunos/', aluno);
     } catch (error) {
       console.log(error);
     }
@@ -97,79 +73,7 @@ export default function AlunosPage() {
         </HeaderContainer>
         {alunos.length > 0 ? (
           <div>
-            <ContainerNewAluno showAddAluno={showAddAluno}>
-              <div className="container">
-                <form onSubmit={handleSubmit} className="formAluno">
-                  <div className="headerForm">
-                    <h1>Adicionar Aluno</h1>
-                    <p>Informe os dados do aluno abaixo:</p>
-                  </div>
-                  <div className="containerInputs">
-                    <ContainerInput>
-                      <label>Nome</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.nome || ''}
-                          id="nome"
-                          type="text"
-                          placeholder="Digite o nome do aluno"
-                        />
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.sobrenome || ''}
-                          id="sobrenome"
-                          type="text"
-                          placeholder="Digite o sobrenome do aluno"
-                        />
-                      </div>
-                    </ContainerInput>
-                    <ContainerInput>
-                      <label>E-mail</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.email || ''}
-                          id="email"
-                          type="email"
-                          placeholder="Digite o e-mail do aluno ou responsavel"
-                        />
-                      </div>
-                    </ContainerInput>
-                    <ContainerInput>
-                      <label>Idade</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.idade || ''}
-                          id="idade"
-                          type="number"
-                          minNumber={1}
-                          maxNumber={120}
-                          placeholder="Digite a idade do aluno"
-                        />
-                      </div>
-                    </ContainerInput>
-                  </div>
-                  <div className="containerButtons">
-                    <button
-                      className="buttonCancel"
-                      type="button"
-                      onClick={() => {
-                        getAluno() + setAluno({});
-                      }}
-                    >
-                      Cancel
-                    </button>
-                    <SubmitButton
-                      text={'Criar Aluno'}
-                      type={'submit'}
-                      variant={'secondary'}
-                    />
-                  </div>
-                </form>
-              </div>
-            </ContainerNewAluno>
+            <AddAlunoForm getAluno={getAluno} showForm={showAddAluno} />
 
             <TableAlunos>
               <thead>
@@ -226,81 +130,7 @@ export default function AlunosPage() {
           </div>
         ) : (
           <div>
-            <ContainerNewAluno showAddAluno={showAddAluno}>
-              <div className="container">
-                <form onSubmit={handleSubmit} className="formAluno">
-                  <div className="headerForm">
-                    <h1>Adicionar Aluno</h1>
-                    <p>Informe os dados do aluno abaixo:</p>
-                  </div>
-                  <div className="containerInputs">
-                    <ContainerInput>
-                      <label>Nome</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.nome || ''}
-                          id="nome"
-                          type="text"
-                          placeholder="Digite o nome do aluno"
-                        />
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.sobrenome || ''}
-                          id="sobrenome"
-                          type="text"
-                          placeholder="Digite o sobrenome do aluno"
-                        />
-                      </div>
-                    </ContainerInput>
-                    <ContainerInput>
-                      <label>E-mail</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.email || ''}
-                          id="email"
-                          type="email"
-                          placeholder="Digite o e-mail do aluno ou responsavel"
-                        />
-                      </div>
-                    </ContainerInput>
-                    <ContainerInput>
-                      <label>Idade</label>
-                      <div className="inputs">
-                        <Input
-                          onChange={handleOnChange}
-                          value={aluno.idade || ''}
-                          id="idade"
-                          type="number"
-                          minNumber={1}
-                          maxNumber={120}
-                          placeholder="Digite a idade do aluno"
-                        />
-                      </div>
-                    </ContainerInput>
-                  </div>
-                  <div className="containerButtons">
-                    <button
-                      className="buttonCancel"
-                      type="button"
-                      onClick={() => {
-                        showAddAluno
-                          ? setShowAddAluno(false) + setAluno({})
-                          : setShowAddAluno(true);
-                      }}
-                    >
-                      Cancel
-                    </button>
-                    <SubmitButton
-                      text={'Criar Aluno'}
-                      type={'submit'}
-                      variant={'secondary'}
-                    />
-                  </div>
-                </form>
-              </div>
-            </ContainerNewAluno>
+            <AddAlunoForm getAluno={getAluno} showForm={showAddAluno} />
 
             <TableAlunos>
               <tbody>
