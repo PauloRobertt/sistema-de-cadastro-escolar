a88f4ce8cc1ce191519a8d1f7d35ae0abe01ee8a
paulorobertt <paulorobert5776@gmail.com>
2025-09-13 01:25:33 -0300

    feat(front-end): implementacao de logout

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/layouts/Menu/input.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/layouts/Menu/input.jsx
@@ -1,17 +1,24 @@
 import { ContainerMenu, ContainerTitle, Nav } from './styled';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import * as actions from '../../store/modules/auth/actions';
 
 //Icons
 import { IoSchoolOutline } from 'react-icons/io5';
 import { CiLogin } from 'react-icons/ci';
 import { CiLogout } from 'react-icons/ci';
-
 import { FiUsers } from 'react-icons/fi';
 import { FiUser } from 'react-icons/fi';
 import { IoHomeOutline } from 'react-icons/io5';
 
+//components
+import SubmitButton from '../../components/SubmitButton';
 import LinkButton from '../../components/LinkButton';
 
 export default function Menu() {
+  const isLogged = useSelector((state) => state.auth.authReducer.isLoggedIn);
+  const dispatch = useDispatch();
+
   return (
     <ContainerMenu>
       <ContainerTitle>
@@ -55,16 +62,32 @@ export default function Menu() {
           </li>
         </ul>
         <ul>
-          <LinkButton
-            variant={'menuLogin'}
-            path={''}
-            text={
-              <>
-                <CiLogout size={'1.3em'} />
-                Log Out // <CiLogin size={'1.3em'} /> Login
-              </>
-            }
-          />
+          <>
+            {isLogged ? (
+              <SubmitButton
+                text={
+                  <>
+                    <CiLogout size={'1.3em'} /> Log Out
+                  </>
+                }
+                variant={'loggout'}
+                onClick={() => {
+                  dispatch(actions.loginError());
+                }}
+                type={'button'}
+              />
+            ) : (
+              <LinkButton
+                variant={'menuLogin'}
+                path={'/login'}
+                text={
+                  <>
+                    <CiLogin size={'1.3em'} /> Login
+                  </>
+                }
+              />
+            )}
+          </>
         </ul>
       </Nav>
     </ContainerMenu>
