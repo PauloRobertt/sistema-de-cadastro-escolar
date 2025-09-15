092175ee8162ea7641359ceeb9c357cd1369ed77
paulorobertt <paulorobert5776@gmail.com>
2025-09-15 17:21:43 -0300

    feat(front-end): ajustes no layout da tela home

--- FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Home/index.jsx
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Home/index.jsx
@@ -8,6 +8,7 @@ import LinkButton from '../../components/LinkButton';
 
 // Icons
 import { IoIosAdd } from 'react-icons/io';
+import { FiUserPlus } from 'react-icons/fi';
 
 //Styles
 import { Container } from '../../styles/GlobalStyles';
@@ -16,11 +17,12 @@ import {
   ContainerSaudacao,
   ContainerCards,
   Cards,
+  ContainerDicasDoSistema,
+  ContainerAtalhos,
 } from './styled';
 
 // Service
 import axios from '../../service/axios';
-import { OrganizarImagens } from '../../assets';
 
 export default function index() {
   const date = new Date();
@@ -47,53 +49,53 @@ export default function index() {
             Bem-vindo,{' '}
             {useSelector((state) => state.auth.authReducer.user.userNome)}!
           </h1>
-          <h3>
+          <p>
             Hoje é {date.getDate()}/{date.getMonth()}/{date.getUTCFullYear()}
-          </h3>
+          </p>
         </ContainerSaudacao>
         <ContainerCards>
-          <div>
-            <Cards>
-              <LinkButton
-                path={''}
-                text={
-                  <>
-                    <IoIosAdd fontSize={'1.5em'} /> Adicionar novo aluno
-                  </>
-                }
-                variant={'adicionarAluno'}
-              />
-            </Cards>
-            <Cards>
-              <h3>Dicas</h3>
-              <ul>
-                <li>
-                  Clique em "cadastrar novo aluno" para adicionar um estudante.
-                </li>
-                <li>
-                  Use "Alunos" no menu lateral para visualizar, editar ou
-                  deletar alunos existentes
-                </li>
-              </ul>
-            </Cards>
-          </div>
-          <div>
-            <Cards>
-              <h3>Total de Alunos</h3>
-              <h1>{alunos.length}</h1>
-            </Cards>
-            <Cards>
-              <h3>Último aluno Cadastro / Editado</h3>
-              {!alunos[0] ? (
-                <p>Nenhum aluno cadastrado</p>
-              ) : (
-                <div>
-                  <p>{alunos[0].nome + ' ' + alunos[0].sobrenome}</p>
-                </div>
-              )}
-            </Cards>
-          </div>
+          <Cards>
+            <h3>Total de Alunos</h3>
+            <h1>{alunos.length}</h1>
+          </Cards>
+          <Cards>
+            <h3>Último aluno Cadastro / Editado</h3>
+            {!alunos[0] ? (
+              <p>Nenhum aluno cadastrado</p>
+            ) : (
+              <div>
+                <p>{alunos[0].nome + ' ' + alunos[0].sobrenome}</p>
+              </div>
+            )}
+          </Cards>
         </ContainerCards>
+        <ContainerDicasDoSistema>
+          <h2>Dicas</h2>
+          <ul style={{ listStyleType: 'circle' }}>
+            <li>
+              Clique em "cadastrar novo aluno" para adicionar um estudante.
+            </li>
+            <li>
+              Use "Alunos" no menu lateral para visualizar, editar ou deletar
+              alunos existentes
+            </li>
+          </ul>
+        </ContainerDicasDoSistema>
+        <ContainerAtalhos>
+          <h2>Ações Rápidas</h2>
+          <p>Acesso rápido às principais funcionalidades do sistema</p>
+          <div>
+            <LinkButton
+              variant={'adicionarAluno'}
+              path={'/alunos'}
+              text={
+                <>
+                  <FiUserPlus size={'1.3em'} /> Cadastrar Novo Aluno
+                </>
+              }
+            />
+          </div>
+        </ContainerAtalhos>
       </ContainerHome>
     </Container>
   );
--- FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Home/styled.js
+++ FrontEnd/Sistema_De_Cadastro_Escolar/src/pages/Home/styled.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
 export const ContainerHome = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: flex-start;
   gap: 1em;
   background-color: #ffff;
   margin-left: 18em;
@@ -11,32 +13,70 @@ export const ContainerHome = styled.div`
   height: 100%;
   padding: 2em;
   border-radius: 1em;
+  p {
+    color: ${colors.textColor};
+  }
 `;
 
-export const ContainerSaudacao = styled.div``;
+export const ContainerSaudacao = styled.div`
+  p {
+    color: ${colors.textColor};
+  }
+`;
 
 export const ContainerCards = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  gap: 1em;
   width: 100%;
-  height: 40%;
-
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: column;
-    gap: 1em;
-  }
+  gap: 2em;
+  flex-wrap: wrap;
 `;
 
 export const Cards = styled.div`
   border: 1px solid #eee;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 15px;
   border-radius: 1em;
-  padding: 1em;
-  min-width: auto;
-  min-height: 120px;
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1em;
+  width: 300px;
+`;
+
+export const ContainerDicasDoSistema = styled.div`
+  width: 100%;
+  border: 1px solid #eee;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 15px;
+  border-radius: 1em;
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 0.5em;
+
+  li {
+    margin: 1em;
+  }
+`;
+
+export const ContainerAtalhos = styled.div`
+  width: 100%;
+  border: 1px solid #eee;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 15px;
+  border-radius: 1em;
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 0.5em;
+
+  div {
+    width: 220px;
+    height: 60px;
+  }
 `;
