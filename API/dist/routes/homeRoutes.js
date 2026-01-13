ef21e60d61df5de6193966ff4f4ff6959cef0db0
paulorobertt <paulorobert5776@gmail.com>
2026-01-12 21:04:14 -0300

    Remoção dos arquivos da pasta dist

--- API/dist/routes/homeRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-
-var _HomeControllerjs = require('../controllers/HomeController.js'); var _HomeControllerjs2 = _interopRequireDefault(_HomeControllerjs);
-
-const router = _express2.default.Router();
-
-router.get('/', _HomeControllerjs2.default.index);
-
-exports. default = router;
--- API/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
-
-const PORT = process.env.PORT || 3000;
-
-_appjs2.default.listen(PORT, () => {
-  console.log(`Servidor rodando no ip: http://localhost:${PORT}`);
-});
